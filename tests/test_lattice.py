ad1ac26b9dd9677e72c4137ad20a71145e8457c7
William Adams <wil.h.adams@gmail.com>
2019-08-06 11:27:14 -0400

    Add tests for 2-D, 3-D Lattice class methods

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -23,6 +23,26 @@ def test_lattice_site():
         too_many_coords = LatticeSite(1, np.array([0., 0., 0., 0.]))
 
 
+def test_linear():
+    test = Lattice.linear(5)
+    assert len(test.sites) == 5
+    assert test.n_sites == 5
+    assert np.allclose(test.adjacency_matrix, np.array([[0,1,0,0,0],[1,0,1,0,0],[0,1,0,1,0],[0,0,1,0,1],[0,0,0,1,0]]))
+    print([site.coords for site in test.sites])
 
 
+def test_rectangular():
+    test = Lattice.rectangular(n_sites=(3, 4), dist=(1., 1.), axis=(0, 1))
+    print(test.adjacency_matrix)
+    print(test.adjacency_matrix.sum(1))
+
+
+def test_oblique():
+    test = Lattice.oblique(n_sites=(3, 4), dist=(1., np.sqrt(2)), axis=(0, 1), angle=(45 * np.pi / 180))
+    print([site.coords for site in test.sites])
+
+
+def test_orthorhombic():
+    test = Lattice.orthorhombic(n_sites=(2, 4, 3), dist=(1., 1., 1.,))
+    print(test.adjacency_matrix.sum(1))
 
