a2b53f1d093f422b2a483356fe94fdc43c190e50
Addison Richards <adsrichards@outlook.com>
2024-04-30 10:24:46 -0400

    fix style for moha_gui

--- moha/gui/moha_gui.py
+++ moha/gui/moha_gui.py
@@ -432,7 +432,6 @@ def make_control_form(frame, ents):
 
 def save_integrals():
     r""" Save integrals to file specified in control fields."""
-
     state_data["control"]["save_integrals"] = True
     set_model(model_ents)
     set_control(control_ents)
