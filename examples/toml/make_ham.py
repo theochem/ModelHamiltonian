dc78fb47e4e9c20d1f6ef78c5bc65b7b3bede6da
= <valerachuiko@gmail.com>
2024-08-16 14:41:09 +0300

    add example of toml usage

---
+++ examples/toml/make_ham.py
@@ -0,0 +1,9 @@
+import moha
+from moha.toml_tools import from_toml
+
+ham = from_toml("ppp.toml")
+zero_body = ham.generate_zero_body_integral()
+one_body = ham.generate_one_body_integral(basis='spatial basis', dense=True)
+two_body = ham.generate_two_body_integral(basis='spatial basis', dense=True)
+
+print(zero_body, one_body.shape, two_body.shape)
