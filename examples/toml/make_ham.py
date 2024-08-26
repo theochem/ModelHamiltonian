117e47aa9ff09d1bf0fa9acaa2c8c2f8b7120a0e
= <valerachuiko@gmail.com>
2024-08-26 14:28:29 +0200

    update examples

--- examples/toml/make_ham.py
+++ examples/toml/make_ham.py
@@ -1,7 +1,7 @@
 import moha
 from moha.toml_tools import from_toml
 
-ham = from_toml("ppp.toml")
+ham = from_toml("hubbard.toml")
 zero_body = ham.generate_zero_body_integral()
 one_body = ham.generate_one_body_integral(basis='spatial basis', dense=True)
 two_body = ham.generate_two_body_integral(basis='spatial basis', dense=True)
